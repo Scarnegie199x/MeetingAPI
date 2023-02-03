5cf2798e48e58b6f0a54041b26a1fdda5d53f3db
Scarnegie199x <65994274+Scarnegie199x@users.noreply.github.com>
2023-02-02 20:37:52 -0700

    Changed Meeting Serializer to nest Topic and display username

--- api/serializers.py
+++ api/serializers.py
@@ -2,13 +2,17 @@ from rest_framework import serializers
 
 from meeting.models import Meeting, Topic
 
-
-class MeetingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Meeting
-        fields = ("title","created_by","created_at","updated_at")
-
 class TopicSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Topic
-        fields = ("title","created_at","updated_at","problem","resolution","meeting")
+        fields = ["title","created_at","updated_at","problem","resolution"]
+
+class MeetingSerializer(serializers.ModelSerializer):
+    created_by = serializers.CharField(source='created_by.username', read_only=True)
+    topic = TopicSerializer(source='Meeting', many=True, read_only=True)
+    class Meta:
+        model = Meeting
+        fields = ["title","created_by","created_at","updated_at","topic"]
+
+
