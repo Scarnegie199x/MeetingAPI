5131e5252dca183c47edd54de59ca7c9de648a8f
Scarnegie199x <65994274+Scarnegie199x@users.noreply.github.com>
2023-02-21 20:21:55 -0700

    Added JS/React for frontend on MeetingsAPI

---
+++ public/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {createRoot} from "react-dom/client";
+
+import TextButtons from "./Button.js"
+
+
+
+function App(){
+    <TextButtons />
+}
+const root = document.querySelector("#react-root");
+createRoot(root).render(React.createElement("", {}, ""));
